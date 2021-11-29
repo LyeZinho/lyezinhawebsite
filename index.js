771c5324055683da0ed4f4de7e20f1fe1ff87d95
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-11-29 23:01:32 +0000

    start commands page

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
+var favicon = require('serve-favicon')
+app.use(favicon(path.join(__dirname, 'favicon', 'favicon.ico')))
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/homepage/home.html'));
-});
+    res.sendFile(path.join(__dirname+'/page/home.html'));
+});  
 
-app.get('/sub', function(req, res) {
-    res.sendFile(path.join(__dirname + '/homepage/home.html'));
+app.get('/commands', function(req, res) {
+    res.sendFile(path.join(__dirname +'/page/commands.html'));
 });
 
 app.get('/ping', function(req, res) {
